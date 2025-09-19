0465c2f3f13b6a406a60aca2529dffab7f1b00dd
Ofir Hafif <p0035442@ness-tech.co.il>
2025-09-19 09:48:38 +0300

    add names of student on landing page + add some word on project

--- client/src/cmp/LandingPage.jsx
+++ client/src/cmp/LandingPage.jsx
@@ -8,8 +8,20 @@ const LandingPage = ({ onStart }) => {
       <img src="../HITLogo.png" alt="HIT Logo" className="HITlogo" />
       <div className="content">
         <h1>WELCOME TO THE SHELTER PROJECT</h1>
-        <p>some words on the project</p>
-        <p>names of the students</p>
+        <p>
+          <ul><li>Shelter Management System</li> <li>"An innovative solution for creating order,
+            supervision, and control over urban shelters."</li>
+          </ul>
+            
+        </p>
+        <p>Participants:</p>
+        <ul>
+          <li>Shir Ganon</li>
+          <li>Adi Beker</li>
+          <li>Sapir Baruch</li>
+          <li>Ofir Hafif</li>
+          <li>Roy Serbi</li>
+        </ul>
         <button className="btn" onClick={onStart}>Get Started</button>
       </div>
     </header>
